baa55a625c54ec2c811e2193fdb8dd90c714fe3b
arthur prod-homme <l3info025@ic2s124-14.univ-lemans.fr>
2018-03-28 12:06:49 +0200

    auth

--- test_Arthur/req.js
+++ test_Arthur/req.js
@@ -1,20 +1,20 @@
-var test = document.getElementById("insta") ;
+/*var test = document.getElementById("insta") ;
 var text = "" ;
-/*
+
 ajaxGet("https://api.instagram.com/v1/users/self/media/recent/?access_token=7206163106.1677ed0.50a736093ff74da59dff518cc556a89d", 
 		function (reponse) {
     
     		var identity = JSON.parse(reponse);
     		//test.innerHTML = identity.data[0].images.thumbnail.width;
     		for(i = 0 ; i < identity.data[0].carousel_media.length ; i++) {
-    			/*var x = document.createElement("IMG") ;
+    			var x = document.createElement("IMG") ;
     			
     			x.src =identity.data[0].carousel_media[i].images.thumbnail.url;
     			
     			test.appendChild(x) ;
     			
     		}
-    		// obtenir infos localisation d'une photo 
+    		/* obtenir infos localisation d'une photo 
     		var lat = identity.data[1].location.latitude ;
     		var long = identity.data[1].location.longitude ;
     		document.getElementById("test").innerHTML = y ;
@@ -42,12 +42,42 @@ ajaxGet("https://api.instagram.com/v1/users/self/media/recent/?access_token=7206
  	 });
 }
 
-function showDiv(id) {
-    document.getElementById(id).style.display = "block" ;
+
+
+function generationLien(id) {
+	var client_id = "9c4fe0163c7545599e409dbb6f1aedf1" ;
+
+	var redirect_url = "http://info.univ-lemans.fr/~l3info025/test_Arthur/Instamap.html" ;
+	
+	var lien = 	"https://api.instagram.com/oauth/authorize/?client_id=" + client_id + "&redirect_uri="+ redirect_url + "&response_type=token" ;
+
+
+	var createA = document.createElement('a') ;
+
+	var createAText = document.createTextNode("Inscription API") ;
+
+	createA.setAttribute('href', lien) ;
+	
+	console.log(lien) ;
+	createA.appendChild(createAText) ;
+
+	var element = document.getElementById(id) ;
+
+	element.appendChild(createA) ;
 }
 
-function notShowDiv(id) {
-    document.getElementById(id).style.display = "none" ;
+
+function accessToken() {
+
+	var url = window.location.href ; // Récupère le lien de la page
+
+	var url_replace = url.replace("#","?") ; // Remplace le "#" par le "?"
+	
+	console.log(url) ;
+
+	var url2 = new URL(url_replace) ;
+
+	var token = url2.searchParams.get("access_token") ;
+	console.log(token) ;
 }
-
-
+	
